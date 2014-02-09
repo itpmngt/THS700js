061c35ae08fb18727807e3e00240275f008ed0d1
Joe Rosa <joe.rosa@itpmngt.co.uk>
2014-02-09 20:08:31 +0000

    Refactor file allocation (css, libs) Add jQuery and jQuery UI Refactor Grunt Initial for to connect Dialog modals

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,19 +18,33 @@ module.exports = function(grunt) {
       build: {
         files: [
           {expand: true, cwd: '', src: ['package.json'], dest: 'builds/src/'},
-          {expand: true, cwd: 'scripts/js/', src: ['serial.js'], dest: 'builds/src/'},
           {expand: true, cwd: 'scripts/html/', src: ['index.html'], dest: 'builds/src/'},
-          {expand: true, cwd: 'builds/nw_modules/', src: ['serialport/**'], dest: 'builds/src/node_modules'}
+          {expand: true, cwd: 'scripts/css/', src: ['style.css'], dest: 'builds/src/'},
+          {expand: true, cwd: 'scripts/js/', src: ['serial.js'], dest: 'builds/src/'}
+          // {expand: true, cwd: 'builds/nw_modules/', src: ['serialport/**'], dest: 'builds/src/node_modules'}
+        ]},
+      libs: {
+        files: [
+          {expand: true, cwd: 'libs/jQuery/', src: ['jquery-2.1.0.min.js'], dest: 'builds/src/'},
+          {expand: true, cwd: 'libs/jQuery UI/jquery-ui-1.9.2.custom/js', src: ['jquery-ui-1.9.2.custom.min.js'], dest: 'builds/src/'},
+          {expand: true, cwd: 'libs/jQuery UI/jquery-ui-1.9.2.custom/css/custom-theme', src: ['jquery-ui-1.9.2.custom.min.css'], dest: 'builds/src/'},
+          {expand: true, cwd: 'libs/jQuery UI/jquery-ui-1.9.2.custom/css/custom-theme', src: ['images/**'], dest: 'builds/src/'}
         ]},
       serialport: {
         files: [
           {expand: true, cwd: 'node_modules/', src: ['serialport/**'], dest: 'builds/nw_modules'}
         ]},
-      nw_enigmavb: {
+      nw_cache: {
         files: [
-          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['*.dll'], dest: 'builds/nw_enigmavb'},
-          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['nw*.*'], dest: 'builds/nw_enigmavb'},
-          {expand: true, cwd: 'builds/webkit/directX', src: ['*.*'], dest: 'builds/nw_enigmavb'}
+          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['*.dll'], dest: 'builds/nw_cache'},
+          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['nw*.*'], dest: 'builds/nw_cache'},
+          {expand: true, cwd: 'builds/webkit/directX', src: ['*.*'], dest: 'builds/nw_cache'}
+        ]},
+      nw_test: {
+        files: [
+          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['*.dll'], dest: 'builds/nw_test'},
+          {expand: true, cwd: 'builds/webkit/cache/win/0.8.4', src: ['nw*.*'], dest: 'builds/nw_test'},
+          {expand: true, cwd: 'builds/webkit/directX', src: ['*.*'], dest: 'builds/nw_test'}
         ]}
       },
     exec: {
@@ -38,8 +52,9 @@ module.exports = function(grunt) {
         rename_zip: { cmd: 'del builds\\temp\\ths700.nw & rename builds\\temp\\ths700.zip ths700.nw' },
         compile_serialport: { cmd: 'cd builds\\nw_modules\\serialport && nw-gyp rebuild --target=0.8.4 && cd ..\\..\\..' },
         // run_ths700: { cmd: 'cd builds\\nw && nw ths700.nw' },
-        join_nwAPP: { cmd: 'copy /b builds\\nw_enigmavb\\nw.exe+builds\\temp\\ths700.nw builds\\temp\\nw.exe' },
-        run_enigma: { cmd: '\"C:\\Program Files\\Enigma Virtual Box\\enigmavb.exe" builds\\enigmavb\\ths700.evb' }
+        join_nwAPP: { cmd: 'copy /b builds\\nw_cache\\nw.exe+builds\\temp\\ths700.nw builds\\nw_test\\nw.exe' },
+        run_app: { cmd: 'builds\\nw_test\\nw.exe' },
+        run_enigma: { cmd: '\"C:\\Program Files\\Enigma Virtual Box\\enigmavb.exe" builds\\dist\\ths700.evb' }
       },
     compress: {
       ths700: {
@@ -54,16 +69,23 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
     'exec:delete_src',    // removes old build
     'copy:build',         // copy files to builds/src
-    'compress:ths700',    // zips the src
+    'copy:libs',          // libraries jQuery, jQueryUI
+    'compress:ths700',    // zips the /src to /temp
     'exec:rename_zip',    // renames the zip file to .nw
     'exec:join_nwAPP',    // joins nw.exe and the ths700.nw
-    'exec:run_enigma']);  // Packaging for Windows using Enigma Virtual Box
+    'exec:run_app' ]);    // Run application
+
+
+  // Packaging for Windows using Enigma Virtual Box
+  grunt.registerTask('enigmavb', ['exec:run_enigma']);
 
   // support tasks
   // This command compiles the module serialport to be used by node-webkit (requires npm install -g nw-gyp)
   grunt.registerTask('compile_serialport', ['copy:serialport','exec:compile_serialport']);
   // puts together all files to pack together with the nw.exe application
-  grunt.registerTask('enigmavb', ['copy:nw_enigmavb']);
+  grunt.registerTask('cache_nw', ['copy:nw_cache']);
+  // puts together all files into a test directory
+  grunt.registerTask('test_nw', ['copy:nw_test']);
 
 
 };
