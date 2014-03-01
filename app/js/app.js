c706ca435df18a137fcbd9c4ce347f3ddae57995
Joe Rosa <joe.rosa@itpmngt.co.uk>
2014-03-01 17:10:17 +0000

    Fix the Angular-routing when using Node-WebKit Refactor app.js Add image for banner (THS700_i32c64.png)

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,10 @@
-angular.module('THS700app', ['ngRoute', 'ngAnimate'],
-  function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/connection', {
-      templateUrl: 'connection.html',
-      controller: connectionCntrll
-    });
-
-    $routeProvider.when('/', {
-      templateUrl: 'banner.html',
-      controller: bannerCntrll
-    });
-
-    // configure html5 to get links working on jsfiddle
-    $locationProvider.html5Mode(true);
+angular
+  .module('THS700app', ['ngRoute', 'ngAnimate'])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/connection', { templateUrl: 'connection.html', controller: connectionCntrll })
+      .when('/main', { templateUrl: 'banner.html', controller: bannerCntrll })
+      .otherwise({redirectTo: '/main'});
+    $locationProvider
+      .html5Mode(false); // cannot use html5 because of Angular-routing
   });
-
-function MainCntl($route, $routeParams, $location) {
-  this.$route = $route;
-  this.$location = $location;
-  this.$routeParams = $routeParams;
-
-  console.log('$route');
-  console.log($route);
-  console.log('$routeParams');
-  console.log($routeParams);
-  console.log('$location');
-  console.log($location);
-}
---
+++ app/js/controllers/main.js
@@ -0,0 +1,12 @@
+function MainCntll($route, $routeParams, $location) {
+  this.$route = $route;
+  this.$location = $location;
+  this.$routeParams = $routeParams;
+
+//  console.log('$route');
+//  console.log($route);
+//  console.log('$routeParams');
+//  console.log($routeParams);
+//  console.log('$location');
+//  console.log($location);
+}
